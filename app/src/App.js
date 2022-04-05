fd81f279665edccacb8a43ab6bbb742c62ca2eb9
MatheusHBitencourt <matheus.svsg@gmail.com>
2022-04-04 22:54:04 -0300

    feat: mvp done

--- app/src/App.js
+++ app/src/App.js
@@ -2,66 +2,49 @@ import "./App.css";
 import {useState} from "react";
 
 function App() {
-  const [salary, setSalary] = useState("");
-  const [hoursPeerWeek, setHoursPeerWeek] = useState("");
-  const [hourPrice, setHourPrice] = useState("");
-  const [productPrice, setProductPrice] = useState("");
-  const [result, setResult] = useState("");
+  const [salary, setSalary] = useState(0);
+  const [hoursPeerWeek, setHoursPeerWeek] = useState(0);
+  const [hourPrice, setHourPrice] = useState(0);
+  const [productPrice, setProductPrice] = useState(0);
+  const [result, setResult] = useState(null);
 
-  function getValues(valueObject) {
-    const valueIdentification = {
-      salary: setSalary(valueObject.salary),
-      hoursPeerWeek: setHoursPeerWeek(valueObject.hoursPeerWeek),
-      hourPrice: setHourPrice(valueObject.hourPrice),
-      productPrice: setProductPrice(valueObject.productPrice),
-    };
-
-    valueIdentification[valueObject.name]();
-  }
 
   function calculate(event) {
     event.preventDefault();
+    const hourPriceCalculation = salary/(hoursPeerWeek*4);
+    const resultCalculation = productPrice/hourPriceCalculation;
+    setResult(resultCalculation);
   }
 
   return (
     <>
       <div>
-        <form>
+        <form onSubmit={calculate}>
           <label>Salary</label>
           <input
             value={salary}
-            onChange={(event) => {
-              getValues({name: "salary", salary: event.target.value});
-            }}
-            type="text"
+            onChange={(event) => {setSalary(event.target.value)}}
+            type="number"
           />
           <label> Worked hours by week</label>
           <input
             value={hoursPeerWeek}
-            onChange={(event) => {
-              getValues({
-                name: "hoursPeerWeek",
-                hoursPeerWeek: event.target.value,
-              });
-            }}
-            type="text"
+            onChange={(event) => {setHoursPeerWeek(event.target.value)}}
+            type="number"
           />
           <label>Desired product price</label>
           <input
-            value={productPrice}
-            onChange={(event) => {
-              getValues({
-                name: "procuctPrice",
-                productPrice: event.target.value,
-              });
-            }}
-            type="text"
+            onChange={(event) => {setProductPrice(event.target.value)}}
+            type="number"
           />
-          <button onSubmit={calculate}>Calculate</button>
+          <button type="submit">Calculate</button>
         </form>
       </div>
+      <div>
+        {result && <h2>You need work {result} hours to get this product </h2>}
+      </div>
     </>
-  ); 
+  );
 }
 
 export default App;
