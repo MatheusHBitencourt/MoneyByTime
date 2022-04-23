fc2659198244acb0859750d4ffe9183cdc55f539
MatheusHBitencourt <matheus.svsg@gmail.com>
2022-04-23 23:21:37 +0000

    feat: frontend update

--- app/src/App.js
+++ app/src/App.js
@@ -1,48 +1,59 @@
 import "./App.css";
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
   const [salary, setSalary] = useState(0);
   const [hoursPeerWeek, setHoursPeerWeek] = useState(0);
-  const [hourPrice, setHourPrice] = useState(0);
   const [productPrice, setProductPrice] = useState(0);
   const [result, setResult] = useState(null);
 
-
   function calculate(event) {
     event.preventDefault();
-    const hourPriceCalculation = salary/(hoursPeerWeek*4);
-    const resultCalculation = productPrice/hourPriceCalculation;
+    const hourPriceCalculation = salary / (hoursPeerWeek * 4);
+    const resultCalculation = productPrice / hourPriceCalculation;
     setResult(resultCalculation);
   }
 
   return (
     <>
-      <div>
+    <header>
+      Money By Hour Calculator
+    </header>
+      <main>
         <form onSubmit={calculate}>
           <label>Salary</label>
           <input
             value={salary}
-            onChange={(event) => {setSalary(event.target.value)}}
+            onChange={(event) => {
+              setSalary(event.target.value);
+            }}
             type="number"
           />
+
           <label> Worked hours by week</label>
           <input
             value={hoursPeerWeek}
-            onChange={(event) => {setHoursPeerWeek(event.target.value)}}
+            onChange={(event) => {
+              setHoursPeerWeek(event.target.value);
+            }}
             type="number"
           />
+
           <label>Desired product price</label>
           <input
-            onChange={(event) => {setProductPrice(event.target.value)}}
+            onChange={(event) => {
+              setProductPrice(event.target.value);
+            }}
             type="number"
           />
+
           <button type="submit">Calculate</button>
         </form>
-      </div>
-      <div>
-        {result && <h2>You need work {result} hours to get this product </h2>}
-      </div>
+
+        <div>
+          {result && <h2>You need work {result} hours to get this product </h2>}
+        </div>
+      </main>
     </>
   );
 }
